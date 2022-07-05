27e49146815d05a070ce128a978f39528c297e00
Topejava1 <topejava@gmail.com>
2022-07-05 06:48:39 -0400

    Initial Commit

---
+++ src/firrsttask.java
@@ -0,0 +1,12 @@
+// Fig.2.1 : Welcome1.java
+//Text-printing program.
+/** Javadoc Comment Enabled here baby */
+
+public class firrsttask
+{
+// main method begins execution of java application.
+public static void main(String[]args)
+        {System.out.println("Welcome to Java Programming!");
+        } // end method main
+        } //end class Welcome1
+
